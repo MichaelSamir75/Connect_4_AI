2c39902a107ec2748e12cab30a64199256b625c8
Mohamed Aiad <88063510+mohamedaiad@users.noreply.github.com>
2022-11-10 04:53:38 +0200

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-final_state_int = self.minimax(cur_state,max_height,0,TRUE) #assume that the agent is the yellow player
-        # final_state = bit_manp.IntToarr2d(final_state_int) 
-        # return final_state
