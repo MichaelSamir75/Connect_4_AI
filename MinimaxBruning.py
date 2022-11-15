4e56fadf6abfe4c9d78b97616960250a3b811876
ahme2001 <ahmedaboeleid30@gmail.com>
2022-11-15 16:02:32 +0200

    no thing useful

--- MinimaxBruning.py
+++ MinimaxBruning.py
@@ -5,14 +5,14 @@ from heuristic import heuristic
 class mimimax_bruning_algorithm:
     # initialize the global variables
     def __init__(self):
-        self.final_result = []
         self.decision_tree = []
         self.values_heuristic = []
 
     def minimax(self,state,max_height,cur_depth,isMax,alpha,beta):
         if(cur_depth == max_height): 
             heur = heuristic()
-            return heur.get_heuristic(state,isMax)
+            x = heur.get_heuristic(state,isMax)
+            return x
 
         final_state = 0
         col = []
@@ -36,12 +36,16 @@ class mimimax_bruning_algorithm:
                 first_empty = first_empty << startDigitOfCol
                 next_state = next_state & ~(7 << startDigitOfCol)  # clear the 3 bit that representing the first empty
                 next_state = next_state | first_empty   # set the first empty in the next state
-                
+                print(f"next state in max {next_state}")
+                if next_state == 0:
+                    print("00000000000000000")
+                    return -math.inf
+                temp = self.minimax(next_state,max_height,cur_depth+1,False,alpha,beta)
                 #add to the tree
                 col.append(next_state)
-
-                temp = self.minimax(next_state,max_height,cur_depth+1,False,alpha,beta)
+                #add to the values of heuristic
                 values.append(temp)
+                
                 if(temp > value): 
                     value = temp
                     final_state = next_state
@@ -50,7 +54,6 @@ class mimimax_bruning_algorithm:
                 if beta <= alpha:
                     break
 
-            self.final_result.append(final_state)
             col.append(final_state)
             values.append(value)
             values.append(isMax)
@@ -64,6 +67,7 @@ class mimimax_bruning_algorithm:
             value = math.inf   #positive infinity     
             #get the min value of the 7 states
             for i in range (7):
+                
                 # get the first empty place in the col and add to it 
                 startDigitOfCol = 9*i
                 x = 7 # 0000 0111
@@ -76,12 +80,17 @@ class mimimax_bruning_algorithm:
                 first_empty = first_empty << startDigitOfCol
                 next_state = next_state & ~(7 << startDigitOfCol)  # clear the 3 bit that representing the first empty
                 next_state = next_state | first_empty   # set the first empty in the next state
+                print(f"next state in min {next_state}")
                 
+                if next_state == 0:
+                    print("tttttttttttttttttt")
+                    return math.inf
+                temp = self.minimax(next_state,max_height,cur_depth+1,True,alpha,beta)
                 #add to the tree
                 col.append(next_state)
-
-                temp = self.minimax(next_state,max_height,cur_depth+1,True,alpha,beta)
+                #add to the values of heuristic
                 values.append(temp)
+                
                 if(temp < value): 
                     value = temp
                     final_state = next_state
@@ -90,11 +99,11 @@ class mimimax_bruning_algorithm:
                 if beta <= alpha:
                     break
 
-            self.final_result.append(final_state)
             col.append(final_state)
             values.append(value)
             values.append(isMax)
-            
+
+            #append to the global arrays 
             self.decision_tree.append(col)
             self.values_heuristic.append(values)
             return value
@@ -102,25 +111,35 @@ class mimimax_bruning_algorithm:
 
 
     def solve(self,state,max_height):
+        
+        #clear the decision tree in every call
+        self.decision_tree.clear()
 
         # convert the current state to int
         bit_manp = bit()
         cur_state = int(bit_manp.arr2dToInt(state))
-        # print(f"final {state}")
+        print(f"final {state}")
+        print(f"int state {cur_state}")
         
         self.minimax(cur_state,max_height,0,True,-math.inf,math.inf) #assume that the agent is the yellow player
-        final_state_int = self.final_result[-1]
+        # the final state is the last element in the decision tree (every state contains 8 states)
+        decision_tree_size = len(self.decision_tree)
+        last_col_decision_tree_size = len(self.decision_tree[decision_tree_size -1])
+        final_state_int = self.decision_tree[decision_tree_size-1][last_col_decision_tree_size-1]
         final_state = bit_manp.IntToarr2d(final_state_int) 
-        # print(f"final {final_state}")
+
+        print(f"final {final_state_int}")
+        print(f"final {final_state}")
+        print()
 
         # the tree begins with the leaves from left to right showing the 7 states and the 8-th of each state represent the node that the heuristic has choosen
-        # for i in range(len(self.decision_tree)):
-        #     for j in range(len(self.decision_tree[i])):
-        #         print(bit_manp.IntToarr2d(self.decision_tree[i][j]), end=" ")
-        #         print(self.values_heuristic[i][j], end = " ")
-        #         if(j==len(self.decision_tree[i]) -1): print(self.values_heuristic[i][j+1], end = " ")    
-        #         print()
-        #     print()
+        for i in range(len(self.decision_tree)):
+            for j in range(len(self.decision_tree[i])):
+                print(bit_manp.IntToarr2d(self.decision_tree[i][j]), end=" ")
+                print(self.values_heuristic[i][j], end = " ")
+                if(j==len(self.decision_tree[i]) -1): print(self.values_heuristic[i][j+1], end = " ")    
+                print()
+            print()
 
 
         return final_state
@@ -129,13 +148,13 @@ class mimimax_bruning_algorithm:
 
 
 
-# state = [[0,0,0,0,0,0,0],
-#         [0,0,0,2,0,1,2],
-#         [2,0,1,2,1,2,1],
+# state = [[1,2,0,1,0,1,0],
+#         [1,1,1,2,2,1,2],
+#         [2,2,1,2,1,2,1],
 #         [1,1,2,2,2,2,1],
 #         [1,2,1,1,1,2,1],
 #         [1,2,2,1,2,1,2]]
 
 # test = mimimax_bruning_algorithm()
-# test.solve(state,4)
+# test.solve(state,3)
 # print()
